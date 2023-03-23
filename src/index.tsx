ad58ef9e98836862961c636285fbe756b2969542
Gabriel Souza <eugabrielf@gmail.com>
2023-03-23 15:29:21 -0300

    styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App"; 
-import "./global.css";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
